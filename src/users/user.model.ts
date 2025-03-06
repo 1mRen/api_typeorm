32e037d2aac5c0ab2a89f182d9b342477f460f46
Marc <mmarclawrence@gmail.com>
2025-03-07 00:52:13 +0800

    UPDATE user.model

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -5,26 +5,27 @@ export class User {
     @PrimaryGeneratedColumn('increment')
     id!: number;
 
-    @Column({ type: "varchar", unique: true, nullable: false })
+    @Column({ type: "varchar", length: 255, unique: true, nullable: false })
     email!: string;
 
-    @Column({ type: "varchar", unique: true, nullable: false })
+    @Column({ type: "varchar", length: 50, unique: true, nullable: false })
     username!: string;
 
-    @Column({ type: "varchar", nullable: false, select: false }) // Exclude by default
+    @Column({ type: "varchar", length: 255, nullable: false, select: false }) // Exclude by default
     passwordHash: string;
 
-    @Column({ type: "varchar", nullable: false })
+    @Column({ type: "varchar", length: 10, nullable: false })
     title: string;
 
-    @Column({ type: "varchar", nullable: false })
+    @Column({ type: "varchar", length: 50, nullable: false })
     firstName: string;
 
-    @Column({ type: "varchar", nullable: false })
+    @Column({ type: "varchar", length: 50, nullable: false })
     lastName: string;
 
-    @Column({ type: "varchar", nullable: false })
+    @Column({ type: "varchar", length: 20, nullable: false })
     role: string;
+
 
     constructor(email: string, username: string,passwordHash: string, title: string, firstName: string, lastName: string, role: string) {
         this.email = email;
